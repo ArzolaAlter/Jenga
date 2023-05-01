77f2dc270d80d844b233bcb40a70b3c7692bb4b8
Arzola <contact@heisarzola.com>
2023-05-01 12:02:55 -0600

    Boot Sequence
    + Implemented the functionality of the game manager.
    + Implemented the boot sequence for the game.

--- Assets/Scripts/Main/GameBrain.cs
+++ Assets/Scripts/Main/GameBrain.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Jenga.API;
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Jenga.Main
 {
@@ -24,24 +25,7 @@ namespace Jenga.Main
     {
         [JsonProperty] public Dictionary<string, List<SubjectData>> GradeToSubject { get; }
 
-        [JsonIgnore]
-        public string HighestGradeSubject
-        {
-            get
-            {
-                List<string> gradesList = GradeToSubject.Keys.ToList();
-                gradesList.Sort((x, y) => String.Compare(y, x, StringComparison.InvariantCulture));
-
-                if (gradesList.Count > 0)
-                {
-                    return gradesList[0];
-                }
-
-                Debug.LogError("The loaded student doesn't have any recorded subjects, and no fallback behaviour has been implemented.");
-                return string.Empty;
-            }
-        }
-
+        [JsonIgnore] public string HighestGradeSubject => GradeToSubject.Keys.ToList().Last(); // This version will just use the order in the API as is, would need to talk for a list of all the possible grades to implement this properly.
         [JsonProperty] public string StudentName => "John Doe";
 
         public JengaData(Dictionary<string, List<SubjectData>> gradeToSubject)
@@ -59,6 +43,18 @@ namespace Jenga.Main
     public class GameBrain : Singleton<GameBrain>
     {
         //------------------------------------------------------------------------------------//
+        /*----------------------------------- FIELDS -----------------------------------------*/
+        //------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Fired the moment the game is actually ready to boot. This mainly means that the game is in a state where student data has already been gathered.
+        /// </summary>
+        public event EventHandler<JengaData> GameReadyToStart;
+
+        // Runtime
+        private JengaData jengaData;
+
+        //------------------------------------------------------------------------------------//
         /*--------------------------------- PROPERTIES ---------------------------------------*/
         //------------------------------------------------------------------------------------//
 
@@ -68,5 +64,58 @@ namespace Jenga.Main
             set => Instances.GameBrain = value;
         }
 
+        //------------------------------------------------------------------------------------//
+        /*---------------------------------- METHODS -----------------------------------------*/
+        //------------------------------------------------------------------------------------//
+
+        private void StartLoginSequence()
+        {
+            // Login Is Skipped on Demo
+
+            GetStudentData();
+        }
+
+        private void GetStudentData()
+        {
+            Instances.APIManager.GetStudentData(response =>
+            {
+                if (response.ResponseResult == UnityWebRequest.Result.Success)
+                {
+                    Dictionary<string, List<SubjectData>> gradeToSubject = new Dictionary<string, List<SubjectData>>();
+                    foreach (SubjectData subject in response.Data)
+                    {
+                        string key = subject.grade.Trim();
+
+                        if (!gradeToSubject.ContainsKey(key))
+                        {
+                            gradeToSubject.Add(key, new List<SubjectData>());
+                        }
+
+                        gradeToSubject[key].Add(subject);
+                    }
+
+                    jengaData = new JengaData(gradeToSubject);
+                    Instances.SaveManager.SaveStudentData(jengaData);
+                }
+                else
+                {
+                    if (!Instances.SaveManager.HasCachedStudentData)
+                    {
+                        // We are in an error state where there is no data to work with.
+                        Debug.LogError("The game is unable to start, no prior connection has been established to load data from.");
+                        return;
+                    }
+
+                    jengaData = Instances.SaveManager.CachedStudentData;
+                }
+
+                GameReadyToStart?.Invoke(this, jengaData);
+            });
+        }
+
+        private void Start()
+        {
+            StartLoginSequence();
+        }
     }
 }
--- Assets/Scripts/Persistence/SaveManager.cs
+++ Assets/Scripts/Persistence/SaveManager.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Jenga.Main;
+using Newtonsoft.Json;
+using UnityEngine;
 
 namespace Jenga.Persistence
 {
     public class SaveManager : Singleton<SaveManager>
     {
         //------------------------------------------------------------------------------------//
+        /*----------------------------------- FIELDS -----------------------------------------*/
+        //------------------------------------------------------------------------------------//
+
+        private const string EXTENSION = ".jenga";
+        private const string STUDENT_CACHE_FILE_NAME = "STUDENT_CACHE";
+
+        //------------------------------------------------------------------------------------//
         /*--------------------------------- PROPERTIES ---------------------------------------*/
         //------------------------------------------------------------------------------------//
+
+        public bool HasCachedStudentData => HasFile(STUDENT_CACHE_FILE_NAME);
+
+        public JengaData CachedStudentData
+        {
+            get
+            {
+                string filePath = GetFilePath(STUDENT_CACHE_FILE_NAME);
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<JengaData>(json);
+            }
+        }
 
         protected override SaveManager InstanceTarget
         {
             get => Instances.SaveManager;
             set => Instances.SaveManager = value;
         }
+
+        //------------------------------------------------------------------------------------//
+        /*---------------------------------- METHODS -----------------------------------------*/
+        //------------------------------------------------------------------------------------//
+
+        private string GetFilePath(string fileId, params string[] folders) => $"{Application.persistentDataPath}{Path.DirectorySeparatorChar}{fileId}{EXTENSION}";
+
+        private bool HasFile(string fileId) => File.Exists(GetFilePath(fileId));
+
+        private void Save<T>(string fileId, T toSave)
+        {
+            string json = JsonConvert.SerializeObject(toSave,
+#if SEEKERS_DEBUG
+                Formatting.Indented
+#else
+                Formatting.None
+#endif
+            );
+            File.WriteAllText(GetFilePath(fileId), json);
+        }
+
+        public void SaveStudentData(JengaData data) => Save(STUDENT_CACHE_FILE_NAME, data);
     }
 }
